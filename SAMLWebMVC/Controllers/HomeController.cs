bcd1fc569afec583b1dd41ca078582f4c7de3bbb
Tommy <dtranthuy1995@gmail.com>
2018-10-17 17:11:47 +0700

    Read SAML Response

--- SAMLWebMVC/Controllers/HomeController.cs
+++ SAMLWebMVC/Controllers/HomeController.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.IdentityModel.Tokens.Saml2;
+using Newtonsoft.Json;
+using Sustainsys.Saml2.Saml2P;
+using System;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
@@ -60,10 +61,34 @@ namespace SAMLWebMVC.Controllers
             }
         }
 
-        public ActionResult About(string response="")
+        public ActionResult About(string SAMLResponse)
         {
-            ViewBag.Message = "Your application description page.";
+            //string samlCertificate = @"-----BEGIN CERTIFICATE-----MIIDSTCCAjGgAwIBAgIEAoLQ/TANBgkqhkiG9w0BAQsFADBVMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxDTALBgNVBAoTBFdTTzIxEjAQBgNVBAMTCWxvY2FsaG9zdDAeFw0xNzA3MTkwNjUyNTFaFw0yNzA3MTcwNjUyNTFaMFUxCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzENMAsGA1UEChMEV1NPMjESMBAGA1UEAxMJbG9jYWxob3N0MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAluZFdW1ynitztkWLC6xKegbRWxky+5P0p4ShYEOkHs30QI2VCuR6Qo4Bz5rTgLBrky03W1GAVrZxuvKRGj9V9+PmjdGtau4CTXu9pLLcqnruaczoSdvBYA3lS9a7zgFU0+s6kMl2EhB+rk7gXluEep7lIOenzfl2f6IoTKa2fVgVd3YKiSGsyL4tztS70vmmX121qm0sTJdKWP4HxXyqK9neolXI9fYyHOYILVNZ69z/73OOVhkh/mvTmWZLM7GM6sApmyLX6OXUp8z0pkY+vT/9+zRxxQs7GurC4/C1nK3rI/0ySUgGEafO1atNjYmlFN+M3tZX6nEcA6g94IavyQIDAQABoyEwHzAdBgNVHQ4EFgQUtS8kIYxQ8UVvVrZSdgyide9OHxUwDQYJKoZIhvcNAQELBQADggEBABfk5mqsVUrpFCYTZZhOxTRRpGXqoW1G05bOxHxs42Paxw8rAJ06Pty9jqM1CgRPpqvZa2lPQBQqZrHkdDE06q4NG0DqMH8NT+tNkXBe9YTre3EJCSfsvswtLVDZ7GDvTHKojJjQvdVCzRj6XH5Truwefb4BJz9APtnlyJIvjHk1hdozqyOniVZd0QOxLAbcdt946chNdQvCm6aUOputp8Xogr0KBnEy3U8es2cAfNZaEkPU8Va5bU6Xjny8zGQnXCXxPKp7sMpgO93nPBt/liX1qfyXM7xEotWoxmm6HZx8oWQ8U5aiXjZ5RKDWCCq4ZuXl6wVsUz1iE61suO5yWi8=-----END CERTIFICATE-----";
+            //Response samlResponse = new Response(samlCertificate);
+            //samlResponse.LoadXmlFromBase64(Request.Form["SAMLResponse"]);
 
+
+            string rawSamlData = Request["SAMLResponse"];
+
+            // the sample data sent us may be already encoded, 
+            // which results in double encoding
+            if (rawSamlData.Contains('%'))
+            {
+                rawSamlData = HttpUtility.UrlDecode(rawSamlData);
+            }
+
+            // read the base64 encoded bytes
+            byte[] samlData = Convert.FromBase64String(rawSamlData);
+
+            // read back into a UTF string
+            string samlAssertion = Encoding.UTF8.GetString(samlData);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(samlAssertion);
+            string json = JsonConvert.SerializeXmlNode(doc);
+            Saml2Id idsamlpe = new Saml2Id("iicipgbjllfjifdjjglpmancpfijenlmpfkbjdeb");
+            var readSamlp = Saml2Response.Read(samlAssertion, idsamlpe);
+
+            ViewBag.JsonExtract = json;
             return View();
         }
 
@@ -95,6 +120,9 @@ namespace SAMLWebMVC.Controllers
             var password = "wso2carbon";
 
             var collection = new X509Certificate2Collection();
+
+            X509Certificate2 cert = new X509Certificate2(System.IO.File.ReadAllBytes(path), "wso2carbon");
+            var a = cert.PrivateKey.ToXmlString(false);
 
             collection.Import(path, password, X509KeyStorageFlags.PersistKeySet);
 
